d410fc144ca578cf9c9f8c143854fa6118f5a82c
iArthurTsai <ibriantsai@gmail.com>
2021-11-09 15:22:47 +0800

    This is my first commit

---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        // This is a comment
+        //lowerCamelCase variable name
+        String myName = "Tsai Arthur";
+        System.out.println("Hello " + myName);
+    }
+}
