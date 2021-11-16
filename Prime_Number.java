63c9b85bb0742d9bf8293593fafdaee7ad2133cb
iArthurTsai <ibriantsai@gmail.com>
2021-11-16 15:56:22 +0800

    This is my first commit

--- Prime_Number.java
+++ Prime_Number.java
@@ -2,7 +2,7 @@ public class Prime_Number {
     public static void main(String[] args) {
         int i, j;
         for (i = 1; i <= 100; i++) {
-            for (j = 2; j < i; j++) {
+            for (j = 2; j < i; j++) {//1既不是質數，所以從2開始
                 if (i % j == 0)
                     break;
             }
