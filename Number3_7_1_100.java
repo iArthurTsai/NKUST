608a48b7b69ff86eea0ab58eed650b0b0f82dc01
iArthurTsai <ibriantsai@gmail.com>
2021-11-16 15:40:54 +0800

    This is my first commit

---
+++ Number3_7_1_100.java
@@ -0,0 +1,9 @@
+public class Number3_7_1_100 {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            if(i%21==0){
+                System.out.println("在1~100可以被21整除的數有"+i+" ");
+            }
+        }
+    }
+}
