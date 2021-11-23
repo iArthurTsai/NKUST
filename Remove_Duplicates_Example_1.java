6ea9d05cb5b93472e68e981f0ec3d2b9ba39cbce
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-11-24 00:06:51 +0800

    initial commit

--- Remove_Duplicates_Example_1.java
+++ Remove_Duplicates_Example_1.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 public class Remove_Duplicates_Example_1 {
     public static void main(String[] args) {
-        ArrayList<String> nums = new ArrayList<>();
-        nums.add("1");
-        nums.add("1");
-        nums.add("2");
+        // Create a HashSet object called nums
+        HashSet<Integer> nums = new HashSet<Integer>();
+        // Add values to the set
+        nums.add(1);
+        nums.add(1);
+        nums.add(2);
         System.out.println("Original ArrayList: " + nums);
-        
+
         //int[] nums = {1, 1, 2}; //Example 1
         //System.out.println("Original: " + Arrays.toString(nums));
         //for (int i=0; i<3; i++) {
