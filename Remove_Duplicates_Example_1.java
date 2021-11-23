b7da8f538ac32c37d8b7e2c8c2d76588ed71d231
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-11-23 23:50:32 +0800

    initial commit

---
+++ Remove_Duplicates_Example_1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Remove_Duplicates_Example_1 {
+    public static void main(String[] args) {
+        ArrayList<String> nums = new ArrayList<>();
+        nums.add("1");
+        nums.add("1");
+        nums.add("2");
+        System.out.println("Original ArrayList: " + nums);
+        
+        //int[] nums = {1, 1, 2}; //Example 1
+        //System.out.println("Original: " + Arrays.toString(nums));
+        //for (int i=0; i<3; i++) {
+            //if (nums[0] == nums[i++]) {
+
+                //System.out.println("Mod: " + Arrays.toString(new int[]{nums[2]}));
+                //}
+        }
+    }
