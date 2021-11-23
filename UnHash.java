162e9b8b4253ed17661bc445e943228f6b671998
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-11-23 15:33:58 +0800

    initial commit

---
+++ UnHash.java
@@ -0,0 +1,31 @@
+class UnHash{
+
+    public static void main (String[] args) {
+        System.out.println(revHash(6933552791181934L));
+        System.out.println(hash("justdoit")); //574318821802
+    }
+
+    static final String ALPHABET = "cdefghijlmnoqstuvxz";
+
+    public static String revHash(long h) {
+        String result = "";
+        while (h>7) {
+            result = ALPHABET.charAt((int)(h%23)) + result;
+            h = h/23;
+        }
+        if (h != 7) {
+            System.err.println("Error, hash parity incorrect.");
+            System.exit(1);
+        }
+        return result;
+        // write the code here
+    }
+
+    public static long hash(String s){
+        long h = 7;
+        for (int i = 0; i < s.length(); i++){
+            h = h * 23 + ALPHABET.indexOf(s.charAt(i));
+        }
+        return h;
+    }
+}
