d313dcf6e179f6f01d75ac03b4feb276a86aa062
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-11-29 23:01:27 +0800

    initial commit

--- All_Factors_of_20.java
+++ All_Factors_of_20.java
@@ -1,10 +1,20 @@
+import java.util.*;
+
 public class All_Factors_of_20 {
     public static void main(String[] args) {
+        HashSet<Integer> nums = new HashSet<Integer>();// Create a HashSet object called nums
+        nums.clear();
+        System.out.println("HashSet: " + nums);
         int n = 20;
         String S;
-        S = n + " 的所有因數";
+        S = n + "的所有因數: ";
         for (int i = 1; i <= n; i++)
             if (n%i == 0)
-                System.out.println(S += (i + "|"));
+                nums.add(i);
+        TreeSet myTreeSet = new TreeSet();
+        myTreeSet.clear();
+        myTreeSet.addAll(nums);
+        System.out.println(S + myTreeSet);// Prints all factors
+        //"輸出20的因數有: "
     }
 }
