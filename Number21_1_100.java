64c3f66c8524e21477c1e555bc7ed9df8f917acb
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-11-29 23:14:20 +0800

    initial commit

--- Number21_1_100.java
+++ Number21_1_100.java
@@ -1,9 +1,18 @@
-public class Number3_7_1_100 {
+import java.util.*;
+
+public class Number21_1_100 {
     public static void main(String[] args) {
+        HashSet<Integer> nums = new HashSet<Integer>();// Create a HashSet object called nums
+        nums.clear();
+        System.out.println("HashSet: " + nums);
         for(int i=1;i<=100;i++){
             if(i%21==0){
-                System.out.println("在1~100可以被21整除的數有"+i+" ");
+                nums.add(i);
             }
         }
+        TreeSet myTreeSet = new TreeSet();
+        myTreeSet.clear();
+        myTreeSet.addAll(nums);
+        System.out.println("在1~100可以被21整除的數有: " + myTreeSet);// Prints all values
     }
 }
