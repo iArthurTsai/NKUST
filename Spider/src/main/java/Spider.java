94dbbaa9278eeaa7470303d3e949b564ec6e0f9f
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-11-30 16:21:33 +0800

    initial commit

--- Spider/src/main/java/Spider.java
+++ Spider/src/main/java/Spider.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Spider {
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+/**
+ * A simple example, used on the jsoup website.
+ */
+public class Spider {
+    public static void main(String[] args) throws IOException {
+        try {
+            Document doc = Jsoup.connect("https://en.m.wikipedia.org/").get();
+            System.out.println("doc.title(): " + doc.title());
+
+            Elements newsHeadlines = doc.select("#mp-itn b a");
+            for (Element headline : newsHeadlines) {
+                System.out.println(headline.attr("title"));
+                System.out.println(headline.absUrl("href"));
+            }
+        } catch (Exception e) {
+            System.out.println("error: " + e);
+        }
+        try {
+            Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
+            System.out.println("doc.title(): " + doc.title());
+
+            Elements newsHeadlines = doc.select("#Dyn_2_2 div.mtitle a");
+            for (Element headline : newsHeadlines) {
+                System.out.println(headline.attr("title"));
+                System.out.println(headline.absUrl("href"));
+            }
+        } catch (Exception e) {
+            System.out.println("error: " + e);
+        }
+    }
 }
