9fed05424e8bb083c7de1c54e569f88911c368b6
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-12-06 22:08:58 +0800

    Example_1&2

--- Remove_Duplicates_Example_1.java
+++ Remove_Duplicates_Example_1.java
@@ -2,20 +2,82 @@ import java.util.*;
 
 public class Remove_Duplicates_Example_1 {
     public static void main(String[] args) {
-        // Create a HashSet object called nums
-        HashSet<Integer> nums = new HashSet<Integer>();
-        // Add values to the set
-        nums.add(1);
-        nums.add(1);
-        nums.add(2);
-        System.out.println("Original ArrayList: " + nums);
 
-        //int[] nums = {1, 1, 2}; //Example 1
-        //System.out.println("Original: " + Arrays.toString(nums));
-        //for (int i=0; i<3; i++) {
-            //if (nums[0] == nums[i++]) {
+        HashSet<String> Example_1 = new HashSet<String>();
 
-                //System.out.println("Mod: " + Arrays.toString(new int[]{nums[2]}));
-                //}
+        // Add numbers to the set
+        Example_1.add("1");
+        Example_1.add("1");
+        Example_1.add("2");
+
+        ArrayList<String> InputExample_1Nums = new ArrayList<String>();
+
+        InputExample_1Nums.add("1");
+        InputExample_1Nums.add("1");
+        InputExample_1Nums.add("2");
+
+        ArrayList<String> OutputExample_1Nums = new ArrayList<String>();
+
+        OutputExample_1Nums.add("1");
+        OutputExample_1Nums.add("2");
+
+        //Remove Duplicates from set
+        for (int i = 0; i < InputExample_1Nums.size() - Example_1.size(); i++) {
+            OutputExample_1Nums.add("_");
         }
+
+        System.out.println("Example_1");
+        System.out.println("Input: " + InputExample_1Nums);
+        System.out.println("k = " + Example_1.size());
+        System.out.println("Output: " + OutputExample_1Nums);
+        System.out.println(Example_1);
+
+
+
+        HashSet<String> Example_2 = new HashSet<String>();
+
+        // Add numbers to the set
+        Example_2.add("0");
+        Example_2.add("0");
+        Example_2.add("1");
+        Example_2.add("1");
+        Example_2.add("1");
+        Example_2.add("2");
+        Example_2.add("2");
+        Example_2.add("3");
+        Example_2.add("3");
+        Example_2.add("4");
+
+        ArrayList<String> InputExample_2Nums = new ArrayList<String>();
+
+        InputExample_2Nums.add("0");
+        InputExample_2Nums.add("0");
+        InputExample_2Nums.add("1");
+        InputExample_2Nums.add("1");
+        InputExample_2Nums.add("1");
+        InputExample_2Nums.add("2");
+        InputExample_2Nums.add("2");
+        InputExample_2Nums.add("3");
+        InputExample_2Nums.add("3");
+        InputExample_2Nums.add("4");
+
+        ArrayList<String> OutputExample_2Nums = new ArrayList<String>();
+
+        OutputExample_2Nums.add("0");
+        OutputExample_2Nums.add("1");
+        OutputExample_2Nums.add("2");
+        OutputExample_2Nums.add("3");
+        OutputExample_2Nums.add("4");
+
+        //Remove Duplicates from set
+        for (int i = 0; i < InputExample_2Nums.size() - Example_2.size(); i++) {
+            OutputExample_2Nums.add("_");
+        }
+
+        System.out.println("Example_2");
+        System.out.println("Input: " + InputExample_2Nums);
+        System.out.println("k = " + Example_2.size());
+        System.out.println("Output: " + OutputExample_2Nums);
+
     }
+}
