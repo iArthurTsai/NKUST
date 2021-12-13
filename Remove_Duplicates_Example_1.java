1947ad0abc53538edbf4d5c47e4b1873b5d3f099
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-12-13 21:56:02 +0800

    訂正答案

--- Remove_Duplicates_Example_1.java
+++ Remove_Duplicates_Example_1.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Remove_Duplicates_Example_1 {
     public static void main(String[] args) {
 
+        //不行透過結果去反推答案
         HashSet<String> Example_1 = new HashSet<String>();
 
         // Add numbers to the set
@@ -79,5 +80,31 @@ public class Remove_Duplicates_Example_1 {
         System.out.println("k = " + Example_2.size());//return k = 5
         System.out.println("Output: " + OutputExample_2Nums);
 
+        //正確答案
+        Map<Character,Integer> checkRepeatMap = new HashMap<>();
+        List<String> outputResult = new ArrayList<>();
+        int firstNumCount = 0 , underscoreCount = 0;
+        //firstNumCount計算第一次出現的數字的數量
+        //underscoreCount計算最後需要加入多少"_"
+        Scanner strInput = new Scanner(System.in);
+        System.out.println("請輸入字串：");
+        String inputNum = strInput.nextLine();
+
+        for (int i = 0 ; i < inputNum.length() ; i++){
+            char getNum = inputNum.charAt(i);               //getNum拿取inputNum裡的數值
+            if (!checkRepeatMap.containsKey(getNum)){       //透過checkRepeatMap判斷數值是否重複
+                outputResult.add(String.valueOf(getNum));   //如不重複將當前數值加進outputResult
+                firstNumCount = firstNumCount + 1;          //並且firstNumCount +1
+            }
+            else{
+                underscoreCount = underscoreCount +1;       //重複的話underscoreCount +1
+            }
+            checkRepeatMap.put(getNum,i);                    //將getNum的值放進checkRepeatMap，用於if判斷是否重複
+        }
+        for (int i = 1 ; i <= underscoreCount ; i++){
+            outputResult.add("_");
+        }
+        System.out.println("nums = " + outputResult);
+        System.out.println("Output：" + firstNumCount);
     }
 }
