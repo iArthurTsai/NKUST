36b66639da4db5636529deda21f26eec5c7975e1
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2021-12-14 15:01:09 +0800

    homework

--- Spider/src/main/java/Spider.java
+++ Spider/src/main/java/Spider.java
@@ -10,30 +10,30 @@ import java.io.IOException;
  */
 public class Spider {
     public static void main(String[] args) throws IOException {
-        try {
-            Document doc = Jsoup.connect("https://en.m.wikipedia.org/").get();
-            System.out.println("doc.title(): " + doc.title());
-
-            Elements newsHeadlines = doc.select("#mp-itn b a");
-            for (Element headline : newsHeadlines) {
-                System.out.println(headline.attr("title"));
-                System.out.println(headline.absUrl("href"));
-            }
-        } catch (Exception e) {
-            System.out.println("error: " + e);
-        }
-        try {
-            Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
-            System.out.println("doc.title(): " + doc.title());
-
-            Elements newsHeadlines = doc.select("#Dyn_2_2 div.mtitle a");
-            for (Element headline : newsHeadlines) {
-                System.out.println(headline.attr("title"));
-                System.out.println(headline.absUrl("href"));
-            }
-        } catch (Exception e) {
-            System.out.println("error: " + e);
-        }
+//        try {
+//            Document doc = Jsoup.connect("https://en.m.wikipedia.org/").get();
+//            System.out.println("doc.title(): " + doc.title());
+//
+//            Elements newsHeadlines = doc.select("#mp-itn b a");
+//            for (Element headline : newsHeadlines) {
+//                System.out.println(headline.attr("title"));
+//                System.out.println(headline.absUrl("href"));
+//            }
+//        } catch (Exception e) {
+//            System.out.println("error: " + e);
+//        }
+//        try {
+//            Document doc = Jsoup.connect("https://rule.nkust.edu.tw/").get();
+//            System.out.println("doc.title(): " + doc.title());
+//
+//            Elements newsHeadlines = doc.select("#Dyn_2_2 div.mtitle a");
+//            for (Element headline : newsHeadlines) {
+//                System.out.println(headline.attr("title"));
+//                System.out.println(headline.absUrl("href"));
+//            }
+//        } catch (Exception e) {
+//            System.out.println("error: " + e);
+//        }
         try {
             Document doc = Jsoup.connect("https://www.ptt.cc/bbs/index.html").get();
             System.out.println(doc.title());
@@ -55,21 +55,22 @@ public class Spider {
             for (Element item1 : items) {//在b-ent裡面抓出board-name和board-title
                 String num = item1.getElementsByClass("board-name").get(0).text();
                 String title = item1.getElementsByClass("board-title").get(0).text();//board-title
+                System.out.println("board>"+item1.getElementsByClass("board-title"));
                 Document doc1 = Jsoup.connect("https://www.ptt.cc/bbs/" + num + "/index.html").get();//從board-name中選擇的素材與網頁組合並進入
-                System.out.println("Board: " + num);
-                System.out.println("Article title: " + title);
+//                System.out.println("Board: " + num);
+//                System.out.println("Article title: " + title);
                 Elements newsHeadlines = doc1.select("#main-container div.r-list-container.action-bar-margin.bbs-screen a");
                 //System.out.println(doc1);
                 for (Element headline : newsHeadlines) {
                     Elements items1 = doc1.getElementsByClass("r-ent");//從doc1中選擇r-ent
                     for (Element item2 : items1) {//從r-ent中逐步搜尋素材
-                        System.out.println(headline.absUrl("href"));
+//                        System.out.println(headline.absUrl("href"));
                         Document doc2 = Jsoup.connect(headline.absUrl("href")).get();//進入Elements newsHeadlines所抓出的href的網址
                         Elements items2 = doc2.getElementsByClass("push");//從href網頁中選擇push
                         for(Element item3 : items2){
                             String num2 = item3.getElementsByClass("push").get(0).getElementsByTag("span").get(2).text();//在class底下選擇第三個span(第一個留言的位置)
                             String num3 = item3.getElementsByClass("push").get(0).getElementsByTag("span").get(1).text();
-                            System.out.println("Comment:" + num3 + num2);
+//                            System.out.println("Comment:" + num3 + num2);
                             break;
                         }
                         break;
@@ -77,7 +78,7 @@ public class Spider {
                     break;
                 }
                 //break;
-                System.out.print("\n");
+//                System.out.print("\n");
             }
             //break;
             //}
