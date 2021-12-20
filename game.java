7e026d7f05848c2332e2ed8d2e7113fcd16d5828
myname <c110181106@nkust.edu.tw>
2021-12-21 01:03:44 +0800

    This is my first commit

---
+++ game.java
@@ -0,0 +1,6 @@
+public class game {
+    public static void main(String[] args){Person Archer = new Person(name:"Archer",personHP:15,personATK:35,personMP:15);Archer.getMyCurrentStatus();
+        Person Berserker = new Person(name:"Berserker",personHP:10,personATK:50,personMP:5 );Berserker.getMyCurrentStatus();
+        System.out.println("---------------------------------")
+    }
+}
