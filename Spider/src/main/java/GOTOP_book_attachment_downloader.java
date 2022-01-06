5aef5f8de7d813590f1c8249da5a0ff7abe43370
bigbettles <c110181102@nkust.edu.tw>
2022-01-07 00:48:10 +0800

    得到書名、封面、作者和ISBN

--- Spider/src/main/java/GOTOP_book_attachment_downloader.java
+++ Spider/src/main/java/GOTOP_book_attachment_downloader.java
@@ -5,11 +5,21 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class GOTOP_book_attachment_downloader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             Document doc = Jsoup.connect("http://books.gotop.com.tw/download/AEI007000").get();
-            System.out.println("書名: " + doc.title());
+            //System.out.println("書名: " + doc.title());
 
+            Elements Title = doc.select("#Label1"); //書名
+            System.out.println("書名：" + Title);
+
+            Elements Image = doc.select("#Image1"); //封面
+            System.out.println("封面：" + Image);
+            Elements Writer = doc.select("#Label2"); //作者
+            System.out.println("作者：" + Writer);
+
+            Elements ISBN = doc.select("#Label3"); //ISBN
+            System.out.println("ISBN：" + ISBN);
 
         } catch (Exception e) {
             System.out.println("error: " + e);
