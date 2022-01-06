3394070bd10adc5e210a779b7eaf0ce0df62e188
myname <c110181106@nkust.edu.tw>
2022-01-06 23:45:15 +0800

    Jsoup.connect

--- Spider/src/main/java/GOTOP_book_attachment_downloader.java
+++ Spider/src/main/java/GOTOP_book_attachment_downloader.java
@@ -1,5 +1,18 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
 public class GOTOP_book_attachment_downloader {
     public static void main(String[] args) {
-        
+        try {
+            Document doc = Jsoup.connect("http://books.gotop.com.tw/download/AEI007000").get();
+            System.out.println("書名: " + doc.title());
+
+
+        } catch (Exception e) {
+            System.out.println("error: " + e);
+        }
     }
 }
