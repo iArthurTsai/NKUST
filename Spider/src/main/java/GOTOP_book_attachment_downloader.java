80f971a4c5beb23ce7066746ca089e71b7c864c1
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2022-01-07 23:35:42 +0800

    讓使用者輸入書號和保留原始檔名

--- Spider/src/main/java/GOTOP_book_attachment_downloader.java
+++ Spider/src/main/java/GOTOP_book_attachment_downloader.java
@@ -1,4 +1,5 @@
 //碁峰圖書附件下載器
+import java.util.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -14,13 +15,19 @@ import java.nio.file.StandardCopyOption;
 public class GOTOP_book_attachment_downloader {
     public static void main(String[] args) throws IOException {
         try {
-            Document doc = Jsoup.connect("http://books.gotop.com.tw/download/AEI005900").get();//for example : AEI005900 AEI005931 AEI006600 AEI007000
+            //https://www.javatpoint.com/how-to-take-string-input-in-java
+            Scanner sc = new Scanner(System.in); //System.in is a standard input stream
+            System.out.print("Enter a Book Number from \"http://books.gotop.com.tw/default.aspx\" : ");
+            String str = sc.next(); //reads string before the space
+            System.out.print("你輸入的書號：" + str); //書號 for example : AEI005900 AEI005931 AEI006600 AEI007000
+
+            Document doc = Jsoup.connect("\"http://books.gotop.com.tw/download/\" + str").get();//for example : AEI005900 AEI005931 AEI006600 AEI007000
             //System.out.println("書名: " + doc.title());
 
             //Elements Title = doc.select("#Label1"); //書名
             //System.out.println("書名：" + Title);
             Element Title = doc.getElementById("Label1"); //書名
-            System.out.println("書名：" + Title.text()); //https://stackoverflow.com/a/19091653
+            System.out.println("\n書名：" + Title.text()); //https://stackoverflow.com/a/19091653
 
 
             Elements Image = doc.select("#Image1"); //封面
@@ -30,10 +37,17 @@ public class GOTOP_book_attachment_downloader {
             for (Element headline : Image) {
                 System.out.println("封面下載網址：" + headline.absUrl("src")); //封面下載網址
 
+                String src =(headline.absUrl("src"));
+
+                //https://www.delftstack.com/zh-tw/howto/java/java-remove-character-from-string/#%E5%9C%A8-java-%E4%B8%AD%E4%BD%BF%E7%94%A8-replace-%E5%87%BD%E5%BC%8F%E5%BE%9E%E5%AD%97%E4%B8%B2%E4%B8%AD%E5%88%AA%E9%99%A4%E5%AD%97%E5%85%83
+                String front_cover = src.replace("http://www.gotop.com.tw/Waweb2004/WawebImages/bookXL/", "");
+
+                System.out.println("File name : " + front_cover);
+
                 URL fetchWebsite = new URL(headline.absUrl("src")); //自動下載封面
                 //https://www.delftstack.com/zh-tw/howto/java/java-downloading-file/#%E5%9C%A8-java-%E4%B8%AD%E4%BD%BF%E7%94%A8-files-copy-%E4%B8%8B%E8%BC%89%E6%AA%94%E6%A1%88
 
-                Path path = Paths.get("front_cover.jpg");
+                Path path = Paths.get(front_cover); //.jpg
                 try (InputStream inputStream = fetchWebsite.openStream()) {
                     Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
                 }
@@ -56,19 +70,34 @@ public class GOTOP_book_attachment_downloader {
             for (Element attachment : newsHeadlines) {
                 System.out.println("附件下載網址：" + attachment.absUrl("href")); //附件下載網址
 
-                Thread      
+                String href =(attachment.absUrl("href"));
+
+                String file_name = href.replace("http://dlcenter.gotop.com.tw/SampleFiles/" + str + "/download/", "");
+                System.out.println("File name : " + file_name);
+
+                //Scanner format = new Scanner(System.in); //System.in is a standard input stream
+                //System.out.print("Enter file format : ");
+                //String file_format = format.next(); //reads string before the space
+                //System.out.print("你輸入的檔案格式：" + file_format + "\n"); //檔案格式
+                System.out.print("Downloading..." + "\n");
+
+
                 URL fetchWebsite = new URL(attachment.absUrl("href")); //自動下載附件
 
-                Path path = Paths.get("file_name.pdf");
+                Path path = Paths.get(file_name); // + "." + file_format
                 try (InputStream inputStream = fetchWebsite.openStream()) {
                     Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
                 }
                 break;
             }
 
+            String Source = ("http://books.gotop.com.tw/download/" + str); //下載來源網址
+            System.out.print("Source : " + Source);
+
         }
         catch (Exception e) {
-            System.out.println("error: " + e);
+            //System.out.println("error: " + e);
+            System.out.println("書號有誤或無附件或檔案名有中文名");
         }
     }
 }
