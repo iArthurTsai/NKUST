4c0b6523f26b54b091a5767f5b276fd6fcb535c8
iArthurTsai <ibriantsai@gmail.com>
2022-01-10 05:16:33 +0800

    example from Web

--- Spider/src/main/java/saveFile.java
+++ Spider/src/main/java/saveFile.java
@@ -1,13 +1,12 @@
-import org.apache.commons.io.FileUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.commons.io.FileUtils;
 
 public class saveFile {
     public static boolean saveFile(URL imgURL, String imgSavePath) {
