9657dedece9efb0c5b97346687db7eae7f672943
bigbettles <c110181102@nkust.edu.tw>
2022-01-11 13:18:12 +0800

    recode log.txt

--- Spider/src/main/java/GOTOP_book_attachment_downloader.java
+++ Spider/src/main/java/GOTOP_book_attachment_downloader.java
@@ -14,17 +14,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.HttpEntity;
 import org.apache.commons.io.FileUtils;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
 //import java.io.InputStream;
 //import java.net.URL;
-//import java.nio.file.Files;
 //import java.nio.file.Path;
-//import java.nio.file.Paths;
 //import java.nio.file.StandardCopyOption;
 
 public class GOTOP_book_attachment_downloader {
     public static void main(String[] args) throws IOException {
         String home = System.getProperty("user.home"); //https://www.796t.com/post/MmNtYzI=.html
+
         try {
 
             //https://www.javatpoint.com/how-to-take-string-input-in-java
@@ -38,6 +38,7 @@ public class GOTOP_book_attachment_downloader {
             System.out.print("\n下載來源網址：" + Source);
 
             String path = (home + "\\Downloads\\" + str + "\\");
+            File log = new File(path + "log.txt"); //https://www.w3schools.com/java/java_files_create.asp
 
             Document doc = Jsoup.connect(Source).get();
             //System.out.println("書名: " + doc.title());
@@ -84,6 +85,11 @@ public class GOTOP_book_attachment_downloader {
                 //try (InputStream inputStream = fetchWebsite.openStream()) {
                 //Files.copy(inputStream, cover, StandardCopyOption.REPLACE_EXISTING);
                 //}
+
+                String CoverToWrite = "\n封面名稱：" + front_cover + "\n封面下載網址：" + corver.absUrl("src");
+                Files.write(Paths.get(path + "log.txt"), CoverToWrite.getBytes()); //https://www.delftstack.com/zh-tw/howto/java/write-a-string-to-a-file-in-java/#%E4%BD%BF%E7%94%A8-java7-%E7%9A%84-files-%E9%A1%9E%E5%B0%87%E5%AD%97%E4%B8%B2%E5%AF%AB%E5%85%A5%E6%AA%94%E6%A1%88
+
+
                 break;
             }
 
@@ -136,11 +142,19 @@ public class GOTOP_book_attachment_downloader {
                 //try (InputStream inputStream = fetchWebsite.openStream()) {
                 //Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
                 //}
+
+                String File_nameToWrite = "\n附件名稱：" + file_name + "\n附件下載網址：" + attachment.absUrl("href");
+                Files.write(Paths.get(path + "log.txt"), File_nameToWrite.getBytes());
+
                 break;
             }
             System.out.println("Downloaded files was saved to " + path);
             java.awt.Desktop.getDesktop().open(new File(home + "\\" + "NKUST_C110181103" + "\\" + "Whistle.mp3"));
             java.awt.Desktop.getDesktop().open(new File(path)); //https://www.itread01.com/p/1434970.html
+
+            String contentToWrite = "你輸入的書號：" + str + "\n下載來源網址：" + Source + "\n書名：" + Title.text() + "\n作者：" + Writer.text() + "\nISBN：" + ISBN.text() + "\nDownloaded files was saved to " + path;
+            Files.write(Paths.get(path + "log.txt"), contentToWrite.getBytes());
+
             System.out.println("Copyright 2077© GOTOP Information Inc, All Rights Reserved 請勿任意連結、轉載"); //http://books.gotop.com.tw/err.html
 
         } catch (Exception e) {
@@ -149,30 +163,6 @@ public class GOTOP_book_attachment_downloader {
             System.out.println("ඞ Youre a sussy baka, i see"); //https://www.codegrepper.com/code-examples/whatever/among+us
             java.awt.Desktop.getDesktop().open(new File(home + "\\" + "NKUST_C110181103" + "\\" + "SussyBaka.m4a"));
             // https://youtu.be/l7yqm4RN7Z4
-        }
-
-        try {
-            String dataKey = "s";
-            String dataVal = "http://books.gotop.com.tw/default.aspx";
-            Document ppt = Jsoup.connect("https://ppt.cc/").data(dataKey, dataVal).userAgent("Mozilla").get();
-
-            Elements pptQRcode = ppt.select("body b a"); //封面
-            System.out.println("QRcode：" + pptQRcode);
-            //Element pptQRcode = ppt.getElementById("href");
-
-            //System.out.println("QRcode：" + pptQRcode);
-            for (Element QRcode : pptQRcode) {
-                System.out.println("QRcode：" + QRcode.absUrl("href")); //封面下載網址
-
-                String src = (QRcode.absUrl("href"));
-
-                //https://www.delftstack.com/zh-tw/howto/java/java-remove-character-from-string/#%E5%9C%A8-java-%E4%B8%AD%E4%BD%BF%E7%94%A8-replace-%E5%87%BD%E5%BC%8F%E5%BE%9E%E5%AD%97%E4%B8%B2%E4%B8%AD%E5%88%AA%E9%99%A4%E5%AD%97%E5%85%83
-                String front_cover = src.replace("http://www.gotop.com.tw/Waweb2004/WawebImages/bookXL/", "");
-
-                //System.out.println("封面名稱：" + front_cover);
-            }
-        } catch (Exception e) {
-            System.out.println("\nerror: " + e);
         }
     }
 }
