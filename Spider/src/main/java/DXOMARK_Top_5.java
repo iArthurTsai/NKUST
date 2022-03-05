c83954c95cdd4968a04516f3331539cd2b9dd748
iArthurTsai <ibriantsai@gmail.comgit config --global user.name iArthurTsai>
2022-03-06 02:25:35 +0800

    added AnTuTu Benchmark

--- Spider/src/main/java/DXOMARK_Top_5.java
+++ Spider/src/main/java/DXOMARK_Top_5.java
@@ -40,7 +40,7 @@ public class DXOMARK_Top_5 {
             System.out.println(doc.title());
 
             Elements rankingTab = doc.getElementsByClass("rankingTab");//從doc中選擇rankingTab裡面
-            for (Element Top_5 : rankingTab) { //在rankingTab裡面抓出title
+            for (Element Top_5 : rankingTab) { //在rankingTab裡面抓出devices
                 Elements devices = doc.getElementsByClass("deviceName flex-col flex-left flex-grow");
                 for (Element Top : devices) { //在devices裡面抓出Device Name
                     String Camera = Top.getElementsByTag("h3").text();
@@ -76,6 +76,51 @@ public class DXOMARK_Top_5 {
             //Elements ISBN = doc.select("#Label3"); //ISBN
             //System.out.println("ISBN：" + ISBN);
             Element ISBN = doc.getElementById("Label3"); //ISBN
+            System.out.println("ISBN：" + ISBN.text());
+
+            System.out.println("Copyright © 2008-2022 DXOMARK. All rights reserved");
+
+        } catch (Exception e) {
+            System.out.println("\nerror: " + e);
+        }
+//#rank > div.mobile-rank > div > div > div.col-xs-12.col-lg-9 > div > div.tab-content > div > ul:nth-child(5) > a > div.nrank-b > li.bfirst > font > font
+//#section1 > div.mainbor.mt2 > div.m_l.fl > ul:nth-child(3) > div.nrank-b > li.bfirst
+        try {
+            Document doc1 = Jsoup.connect("https://www.antutu.com/en/ranking/rank1.htm").get();
+            System.out.println(doc1.title());
+            Elements devices = doc1.getElementsByClass("nrank-b");
+            for (Element Name : devices) { //在devices裡面抓出Device Name
+                Elements newsHeadlines = doc1.getElementsByClass("bfirst");
+                String N = Name.getElementsByTag("li").text();
+                System.out.println(N); //網址
+                for (Element attachment : newsHeadlines) {
+                    //String href = attachment.absUrl("href");
+                    //System.out.println(N); //網址
+                }
+            }
+            Elements Image = doc1.select("#Image1"); //封面
+            //System.out.println("封面：" + Image);
+            //Element Image = doc.getElementById("Image1");
+            //System.out.println("封面：" + Image);
+            for (Element corver : Image) {
+                System.out.println("封面下載網址：" + corver.absUrl("src")); //封面下載網址
+
+                String src = corver.absUrl("src");
+
+                //https://www.delftstack.com/zh-tw/howto/java/java-remove-character-from-string/#%E5%9C%A8-java-%E4%B8%AD%E4%BD%BF%E7%94%A8-replace-%E5%87%BD%E5%BC%8F%E5%BE%9E%E5%AD%97%E4%B8%B2%E4%B8%AD%E5%88%AA%E9%99%A4%E5%AD%97%E5%85%83
+                String front_cover = src.replace("http://www.gotop.com.tw/Waweb2004/WawebImages/bookXL/", "");
+
+                System.out.println("封面名稱：" + front_cover);
+            }
+
+            //Elements Writer = doc.select("#Label2"); //作者
+            //System.out.println("作者：" + Writer);
+            Element Writer = doc1.getElementById("Label2"); //作者
+            System.out.println("作者：" + Writer.text());
+
+            //Elements ISBN = doc.select("#Label3"); //ISBN
+            //System.out.println("ISBN：" + ISBN);
+            Element ISBN = doc1.getElementById("Label3"); //ISBN
             System.out.println("ISBN：" + ISBN.text());
 
             System.out.println("Copyright © 2008-2022 DXOMARK. All rights reserved");
